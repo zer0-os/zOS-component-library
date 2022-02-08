fec33df410b1b3d0255f32495e395e48f15ea3cc
Joel Tulloch <joel@jtulloch.com>
2022-02-08 16:01:05 -0500

    adds top level exports

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { Button } from './components/button';
+export { Dialog } from './components/dialog';
+export { EthAddress } from './components/eth-address';
+export { WalletSelect } from './components/wallet-select';
+export {
+  Context as EscapeManagerContext,
+  Provider as EscapeManagerProvider,
+} from './lib/escape-manager';
