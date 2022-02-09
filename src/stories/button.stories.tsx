9d4b38e06937476cfb3928de41ad7ad73125b986
Joel Tulloch <joel@jtulloch.com>
2022-02-09 09:28:50 -0500

    adds stories

---
+++ src/stories/button.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from '../components/button';
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    className: 'storybook-button',
+  },
+} as ComponentMeta<typeof Button>;
+
+export const WithLabel = (args) => <Button label='Launch!' {...args} />;
+export const WithChild = (args) => (
+  <Button {...args}>
+    <div className='storybook-button__child'>Click Me!!!</div>
+  </Button>
+);
---
+++ src/stories/wallet-select.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+
+import { WalletSelect } from '../components/wallet-select';
+
+export default {
+  title: 'Wallet Select',
+  component: WalletSelect,
+  args: {
+    className: 'storybook-wallet-select',
+  },
+} as ComponentMeta<typeof WalletSelect>;
+
+export const WithDefaultWallets = (args) => <WalletSelect {...args} />;
