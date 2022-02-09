6583df4c788ae7a422778f999289d305dc20d630
Joel Tulloch <joel@jtulloch.com>
2022-02-09 14:57:26 -0500

    wallet type is exported

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Button } from './components/button';
 export { Dialog } from './components/dialog';
 export { EthAddress } from './components/eth-address';
+export { WalletType } from './components/wallet-select/wallets';
 export { WalletSelect } from './components/wallet-select';
 export { WalletSelectModal } from './components/wallet-select/modal';
 export {
