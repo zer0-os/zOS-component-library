6845b614d101cf49f05e1463001e9af956b60a12
Joel Tulloch <joel@jtulloch.com>
2022-02-09 14:51:47 -0500

    modal is also exported

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Button } from './components/button';
 export { Dialog } from './components/dialog';
 export { EthAddress } from './components/eth-address';
 export { WalletSelect } from './components/wallet-select';
+export { WalletSelectModal } from './components/wallet-select/modal';
 export {
   Context as EscapeManagerContext,
   Provider as EscapeManagerProvider,
