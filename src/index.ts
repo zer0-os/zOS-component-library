f5584160cde9e25bb8be97f62ebfc2db70240945
Joel Tulloch <joel@jtulloch.com>
2022-04-20 15:56:25 -0600

    export Icons enum

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { IconButton } from './components/icon-button';
+export { Icons } from './components/icon-button/icons';
 export { Button } from './components/button';
 export { Dialog } from './components/dialog';
 export { EthAddress } from './components/eth-address';
