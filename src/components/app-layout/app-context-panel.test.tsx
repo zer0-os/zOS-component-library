aa14670f89aa530579fb1fc1603b0c2e003b40ca
Joel Tulloch <joel@jtulloch.com>
2022-09-26 13:02:43 -0400

    add open/closed classes to context panel

--- src/components/app-layout/app-context-panel.test.tsx
+++ src/components/app-layout/app-context-panel.test.tsx
@@ -12,6 +12,22 @@ describe('AppContextPanel', () => {
   it('renders children', () => {
     const wrapper = subject(<div className='tacos' />);
 
-    expect(wrapper.find('.app-context-panel .tacos').exists()).toBe(true);
+    expect(wrapper.find('.app-context-panel__content .tacos').exists()).toBe(true);
+  });
+
+  it('adds class when target is clicked', () => {
+    const wrapper = subject();
+
+    wrapper.find('.app-context-panel__target').simulate('click');
+
+    expect(wrapper.find('.app-context-panel').hasClass('open')).toBe(true);
+  });
+
+  it('has closed class when rendered by default', () => {
+    const wrapper = subject();
+
+    const panel = wrapper.find('.app-context-panel');
+
+    expect(panel.hasClass('closed')).toBe(true);
   });
 });
--- src/components/app-layout/app-context-panel.tsx
+++ src/components/app-layout/app-context-panel.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import classNames from 'classnames';
 
-export class AppContextPanel extends React.Component {
+interface State {
+  isOpen: boolean;
+}
+
+export class AppContextPanel extends React.Component<{}, State> {
+  state = { isOpen: false };
+
+  handleClick = () => {
+    this.setState({ isOpen: true });
+  }
+
   render() {
-    return <div className='app-context-panel'>{this.props.children}</div>;
+    const className = classNames('app-context-panel', {
+      'open': this.state.isOpen,
+      'closed': !this.state.isOpen,
+    });
+
+    return (
+      <div className={className}>
+        <div className='app-context-panel__target' onClick={this.handleClick} />
+        <div className='app-context-panel__content'>
+          {this.props.children}
+        </div>
+      </div>
+    );
   }
 }
