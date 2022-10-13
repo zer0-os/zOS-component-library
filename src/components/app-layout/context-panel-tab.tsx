3c992f159d8b2c5eb638ce6193268b42b934983c
Joel Tulloch <joel@jtulloch.com>
2022-10-13 12:54:05 -0400

    adds a gradient to the panel tab

--- src/components/app-layout/context-panel-tab.tsx
+++ src/components/app-layout/context-panel-tab.tsx
@@ -15,6 +15,13 @@ export class ContextPanelTab extends React.Component<Properties> {
     return (
       <div className={className} onClick={this.handleClick}>
         <svg className='context-panel-tab__tab' viewBox="0 0 16 104" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient id="context-panel-tab__opacity-gradient">
+              <stop className="context-panel-tab__opacity-gradient-first-stop" offset="0" />
+              <stop className="context-panel-tab__opacity-gradient-third-stop" offset="45%" />
+              <stop className="context-panel-tab__opacity-gradient-second-stop" offset="100%" />
+            </linearGradient>
+          </defs>
           <path d="M4.03079 89.9538L11.9692 78.0462C14.5975 74.1038 16 69.4716 16 64.7334V51.5V39.2666C16 34.5284 14.5975 29.8962 11.9692 25.9538L4.03079 14.0462C1.40251 10.1038 0 5.47158 0 0.733383V51.5V103.267C0 98.5284 1.40251 93.8962 4.03079 89.9538Z" />
         </svg>
         <svg className='context-panel-tab__icon' viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
